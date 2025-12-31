34dc9cfcc9d76631938d2de1356a450061af1136
Javier Giner <jlginer@hotmail.com>
2025-12-31 17:33:54 -0300

    arcane

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import InstallGate from "./components/InstallGate";
 import NormalBoard from "./screens/NormalBoard";
 import EasyBoard from "./screens/EasyBoard";
 import Home from "./screens/Home";
@@ -7,14 +5,8 @@ import { TextosProvider } from "./contexts/LanguageContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [skipInstall, setSkipInstall] = useState(false);
-
   return (
     <TextosProvider>
-      {/* INSTALL GATE */}
-      {!skipInstall && <InstallGate onContinue={() => setSkipInstall(true)} />}
-
-      {/* APP REAL */}
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
